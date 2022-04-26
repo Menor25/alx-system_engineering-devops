8da1e299e09ae887d123f9dfe8023b3e637321db
Menor25 <ajirimenor@gmail.com>
2022-04-26 19:25:29 +0100

    script that queries the Reddit API and returns the number of subscribers to the subreddits

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,14 @@
 """Module that queries the reddit api"""
 
 
-
 def number_of_subscribers(subreddit):
     """script that queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-
     import requests
 
     query_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
-                            headers={"User-Agent": "My-Agent"},
+                            headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
     if query_info.status_code >= 300:
         return 0
