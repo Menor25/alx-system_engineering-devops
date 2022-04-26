8c2b77d1cc466619ba712e2727ae001fd8452a39
Menor25 <ajirimenor@gmail.com>
2022-04-26 19:35:32 +0100

    script that queries the Reddit API and returns the number of the first 10 hot post to the subreddits

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,14 +2,13 @@
 """Module that queries the reddit api"""
 
 
-python = subreddit
-def top_ten(python):
+def top_ten(subreddit):
     """script that queries the Reddit API and returns the top 10 hot post
     to the subreddit"""
     import requests
 
     query_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(python),
+                            .format(subreddit),
                             headers={"User-Agent": "My-Agent"},
                             allow_redirects=False)
     if query_info.status_code >= 300:
